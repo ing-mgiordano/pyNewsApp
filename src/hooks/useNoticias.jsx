3cd6401289262ed11fe407baccf632ec1492422f
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-21 20:51:38 +0200

    se creo Provider y hook

---
+++ src/hooks/useNoticias.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react"
+import NoticiasContext from "../context/NoticiasProvider"
+
+const useNoticias = () => {
+
+
+    return (
+        useContext(NoticiasContext)
+    )
+}
+
+export default useNoticias
