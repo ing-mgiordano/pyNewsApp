e7124f1c2b430adff2cceab6db19dea504869745
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-22 11:30:57 +0200

    se muestran resultados

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,4 +1,4 @@
-
+import { Card, CardActions, CardContent, CardMedia, Link, Typography, Grid } from "@mui/material"
 
 const Noticia = ({noticia}) => {
 
@@ -7,9 +7,45 @@ const Noticia = ({noticia}) => {
     const {urlToImage, url, title, description, source} = noticia
 
     return (
-        <div>
-        
-        </div>
+        <Grid item md={6} lg={4}>
+            <Card>
+                {urlToImage && (
+                    <CardMedia 
+                        component='img'
+                        alt={`imagen de la noticia ${title}`}
+                        image={urlToImage}
+                        height={'250'}
+                    />
+                )}
+
+                <CardContent>
+                    <Typography variant='body1' color='error'>
+                        {source.name}
+                    </Typography>
+                    <Typography variant='h5' component='div'>
+                        {title}
+                    </Typography>
+                    <Typography variant='body2'>
+                        {description}
+                    </Typography>
+                </CardContent>
+
+                <CardActions>
+                   <Link
+                        href={url}
+                        target='_blank'
+                        variant='button'
+                        width='100%'
+                        textAlign='center'
+                        sx={{
+                            textDecoration: 'none'
+                        }}
+                   >
+                        Leer Noticia
+                   </Link> 
+                </CardActions>
+            </Card>
+        </Grid>
     )
 }
 
